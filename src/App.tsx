6e9a12c65b7f23b54f81d3054d067954b7a98fbb
@a.ilin <a.ilin@invitro.com>
2025-05-21 10:22:39 +0300

    вёрстка

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>
-    <Button>
-      click 2
-    </Button>
-  </div>;
+  return (
+    <div className="bg-blue-50 w-full h-dvh p-8">
+      <div className="max-w-3xl mx-auto flex gap-4">
+        <nav className="bg-white rounded-2xl px-8 py-6">
+          <ul className="flex flex-col gap-4">
+            <li className="text-neutral-500 p-2 hover:bg-neutral-100 rounded-2xl">
+              Поиск
+            </li>
+            <li>Создание</li>
+          </ul>
+        </nav>
+        <main className="bg-white rounded-2xl p-6 w-full">
+          <input type="text" />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
